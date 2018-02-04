f0cfec22362fd3857b2fd161ef73b32dbf160032
Timothy Barland <tbarland77@hotmail.com>
2018-02-04 10:56:16 -0500

    added custom list view and using viewholder

--- app/src/main/java/com/programmerz/timothy/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/programmerz/timothy/coderswag/controller/MainActivity.kt
@@ -2,23 +2,20 @@ package com.programmerz.timothy.coderswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import com.programmerz.timothy.coderswag.R
-import com.programmerz.timothy.coderswag.model.Category
+import com.programmerz.timothy.coderswag.adapters.CategoryAdapter
 import com.programmerz.timothy.coderswag.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
     }
