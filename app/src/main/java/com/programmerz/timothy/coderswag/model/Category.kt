4678425479e67bd341a3f97e6c750d7b61d12787
Timothy Barland <tbarland77@hotmail.com>
2018-02-04 07:36:53 -0500

    added list view

--- app/src/main/java/com/programmerz/timothy/coderswag/model/Category.kt
+++ app/src/main/java/com/programmerz/timothy/coderswag/model/Category.kt
@@ -3,4 +3,8 @@ package com.programmerz.timothy.coderswag.model
 /**
  * Created by Timothy on 2/4/2018.
  */
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
